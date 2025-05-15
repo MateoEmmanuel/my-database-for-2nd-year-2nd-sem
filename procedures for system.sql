ab11d83c503734420d14a7a84815e7c36d32f4c7
MateoEmmanuel <mateoemmanuelwalter11@gmail.com>
2025-05-15 20:20:03 +0800

    Update procedures for system.sql

--- procedures for system.sql
+++ procedures for system.sql
@@ -21,14 +21,43 @@ CREATE PROCEDURE GetProfileImageByRole (
 )
 BEGIN
     IF p_role = 'Customer' THEN
-        SELECT IL.image_path
+        SELECT IL.image
         FROM customer_accounts CA
         JOIN image_list IL ON CA.image_id = IL.image_id
         WHERE CA.customer_id = p_id;
     ELSE
-        SELECT IL.image_path
+        SELECT IL.image
         FROM restaurant_accounts RA
         JOIN image_list IL ON RA.image_id = IL.image_id
         WHERE RA.account_id = p_id;
     END IF;
+END //
+
+CREATE PROCEDURE getaccountinfo_personel(
+    IN p_id INT,
+    OUT first_name VARCHAR(50),
+    OUT middle_name VARCHAR(50),
+    OUT last_name VARCHAR(50),
+    OUT contact_number VARCHAR(15),
+    OUT email VARCHAR(100),
+    OUT Position_name VARCHAR(100)
+)
+BEGIN
+    SELECT 
+        ra.first_name,
+        ra.middle_name,
+        ra.last_name,
+        ra.contact_number,
+        ra.email,
+        pl.Position_name
+    INTO 
+        first_name,
+        middle_name,
+        last_name,
+        contact_number,
+        email,
+        Position_name
+    FROM restaurant_accounts ra
+    JOIN Position_List pl ON ra.position_id = pl.Position_ID
+    WHERE ra.account_id = p_id;
 END //
